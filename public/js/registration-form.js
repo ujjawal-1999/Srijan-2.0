38e30b1f53d6990e50f66be355619c38acf440d2
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2021-03-21 21:12:21 +0530

    dynamic price complete

--- public/js/registration-form.js
+++ public/js/registration-form.js
@@ -1,9 +1,35 @@
-var wsPrice = [300, 300, 300, 300];
-var tsPrice = 350;
+const wsPrice = [300, 300, 300, 300];
+const tsPrice = 350;
+
+var priceToBePaid = 0;
+
 var tsCheckbox = document.querySelector('#tsCheckbox');
+var wsCheckbox = document.querySelectorAll(".ws");
 
-tsCheckbox.addEventListener('click', function(){
-    console.log(tsCheckbox.checked)
+var priceDisplay = document.querySelector('.amount');
 
-})
+function changePrice(){
+    if(this.id == 'tsCheckbox'){
+        if(this.checked)
+        priceToBePaid += tsPrice;
+        else
+        priceToBePaid -= tsPrice;
+    }
+    else{
+        if(this.checked)
+        priceToBePaid += wsPrice[ parseInt(this.id) - 1 ]
+        else
+        priceToBePaid -= wsPrice[ parseInt(this.id) - 1 ]
+    }
+    priceDisplay.innerHTML =   `INR ${priceToBePaid}`;
+}
 
+changePrice.call(tsCheckbox);
+
+for(var i=0; i<wsCheckbox.length; i++){
+    var ws = wsCheckbox[i];
+
+    ws.addEventListener('click', changePrice );
+}
+
+tsCheckbox.addEventListener('click', changePrice );
