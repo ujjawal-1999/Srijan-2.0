116838a309f7f27201c2d14bb4201871db7b0e47
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2021-03-21 20:16:24 +0530

    reg. form js added

---
+++ public/js/registration-form.js
@@ -0,0 +1,9 @@
+var wsPrice = [300, 300, 300, 300];
+var tsPrice = 350;
+var tsCheckbox = document.querySelector('#tsCheckbox');
+
+tsCheckbox.addEventListener('click', function(){
+    console.log(tsCheckbox.checked)
+
+})
+
