6043b5006d8369dd5920740470456fae127dbc56
Ujjawal Jain <jainujjawal1999@gmail.com>
2021-03-28 22:08:38 +0530

    Modified Payment Schema

--- src/models/payment.js
+++ src/models/payment.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const PaymentSchema = new mongoose.Schema({
   data: {
     type: Object,
-  },
+  }
+},{
+  timestamps: true,
 });
 
 module.exports = Payment = mongoose.model("PaymentSchema", PaymentSchema);
