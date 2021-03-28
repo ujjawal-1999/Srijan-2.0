4dd4030b1a039516b64f585e814a7b970082f1dd
Ujjawal Jain <jainujjawal1999@gmail.com>
2021-03-28 13:33:19 +0530

    Sends mail on notify button click

--- src/routers/notify.js
+++ src/routers/notify.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const NotifyEmailSchema = require("../models/notify");
+const {contact} = require('../nodemailer/mail');
 
 router.post("/", async (req, res) => {
   try {
     let email = req.body.email;
+    email = email.toLowerCase();
     let alreadyExistingEmail = await NotifyEmailSchema.findOne({
       email,
     });
     if (alreadyExistingEmail) {
+      contact(email);
       res.redirect("/");
       return;
     }
     let user = new NotifyEmailSchema({
-      email: email.toLowerCase(),
+      email
     });
-
+    contact(email);
     await user.save();
+
     res.redirect("/");
   } catch (error) {
     console.log(error);
